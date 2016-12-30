f4ff991ab3ca93e6309f935a0d2bcbda1960522a
HJK <hjk_sports@126.com>
2016-12-30 13:38:47 +0800

    新增程序入口及测试类

---
+++ src/main/java/indi/hjk/codemaker/App.java
@@ -0,0 +1,11 @@
+package indi.hjk.codemaker;
+
+/**
+ * 程序入口
+ * Created by HJK on 2016/12/30.
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/indi/hjk/codemaker/AppTest.java
@@ -0,0 +1,15 @@
+package indi.hjk.codemaker;
+
+import org.junit.Test;
+
+/**
+ * Created by HJK on 2016/12/30.
+ */
+public class AppTest {
+
+    @Test
+    public void test(){
+        App app=new App();
+
+    }
+}
